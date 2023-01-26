9678f7338d616156a75b2ec3194fbb2de077be4a
Juangmcz <juangui-089@hotmail.com>
2023-01-26 12:30:32 -0500

    Exercise 1 of WarmingUp class is done.

--- src/warmingup/WarmingUp.java
+++ src/warmingup/WarmingUp.java
@@ -8,27 +8,44 @@ import java.util.stream.Collectors;
 
 public class WarmingUp {
     public static void main(String[] args) {
-        System.out.println(lengthMap(List.of("apple","melon","pinneapple","mango","lemon","blueberry","banana","strawberry","kiwi","maracuya","pitahaya")));
+
         /* TO DO:
-        * Given a list of integers, use the Stream API to filter out even numbers, square the remaining numbers, and return the sum of the squared numbers.
+        * Exercise 1: Given a list of integers, use the Stream API to filter out even numbers, square the remaining numbers, and return the sum of the squared numbers.
 
-        * Given a list of strings, use the Stream API to filter out strings that have a length greater than 5, convert the remaining strings to uppercase, and return a new list of the modified strings.
+        * Exercise 2: Given a list of strings, use the Stream API to filter out strings that have a length greater than 5, convert the remaining strings to uppercase, and return a new list of the modified strings.
 
-        * Given a list of strings, use the Stream API to group the strings by their length, and return a map where the keys are the lengths and the values are lists of strings with that length.
+        * Exercise 3: Given a list of strings, use the Stream API to group the strings by their length, and return a map where the keys are the lengths and the values are lists of strings with that length.
 
-        * Given a list of strings, use the Stream API to group the strings by their first letter, and return a map where the keys are the first letters and the values are lists of strings that start with that letter.
+        * Exercise 4: Given a list of strings, use the Stream API to group the strings by their first letter, and return a map where the keys are the first letters and the values are lists of strings that start with that letter.
 
-        * Given a list of people, use the Stream API to group the people by their age, and return a map where the keys are the ages and the values are lists of people with that age.
+        * Exercise 5: Given a list of people, use the Stream API to group the people by their age, and return a map where the keys are the ages and the values are lists of people with that age.
 
-        * Given a list of strings, use the Stream API to filter out strings that have a length less than 4, reverse the remaining strings, and return a new list of the modified strings.
+        * Exercise 6: Given a list of strings, use the Stream API to filter out strings that have a length less than 4, reverse the remaining strings, and return a new list of the modified strings.
 
-        * Given a list of people, use the Stream API to filter out people who are older than 40, sort the remaining people by their age in descending order, and return a new list of the modified people.
+        * Exercise 7: Given a list of people, use the Stream API to filter out people who are older than 40, sort the remaining people by their age in descending order, and return a new list of the modified people.
 */
+
+        // Solving exercise 1:
+        System.out.println(exercise1());
+
+        // Solving exercise 2:
+        System.out.println(exercise2());
     }
 
-    public static Map<Integer,List<String>> lengthMap(List<String> words){
-        //First I need to know the length of the words
-        return words.stream().collect(Collectors.groupingBy(String::length));
+    public static int exercise1() {
 
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        return numbers.stream()
+                .filter(n -> n % 2 != 0) // filter out even numbers
+                .map(n -> n * n) // square the remaining numbers
+                .reduce(0, Integer::sum); // sum the squared numbers
+    }
+
+    public static List<String> exercise2(){
+
+        return null;
     }
 }
+
+
