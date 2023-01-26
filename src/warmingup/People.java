b530b695e631bf67edb4ca1d8999344069711e45
Jonathan Pinilla <jonathan_pinilla@outlook.com>
2023-01-26 15:15:13 -0500

    All seventh tasks completed and working

--- src/warmingup/People.java
+++ src/warmingup/People.java
@@ -3,5 +3,38 @@ package warmingup;
 public class People {
     private String name;
     private int age;
-    private String gender; //M, F
+    private Gender gender; //M, F, O
+
+    enum Gender{
+        M,
+        F,
+        O
+    }
+
+    public People(String name, int age, Gender gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                '}';
+    }
 }
--- src/warmingup/WarmingUp.java
+++ src/warmingup/WarmingUp.java
@@ -1,69 +1,115 @@
 package warmingup;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class WarmingUp {
     public static void main(String[] args) {
 
-        /* TODO:
-        * Given a list of integers, use the Stream API to filter out even numbers, square the remaining numbers, and return the sum of the squared numbers.
+        /*
+         * 1 Given a list of integers, use the Stream API to filter out even numbers, square the remaining numbers, and return the sum of the squared numbers.
 
-        * Given a list of strings, use the Stream API to filter out strings that have a length greater than 5, convert the remaining strings to uppercase, and return a new list of the modified strings.
+         * 2 Given a list of strings, use the Stream API to filter out strings that have a length greater than 5, convert the remaining strings to uppercase, and return a new list of the modified strings.
 
-        * Given a list of strings, use the Stream API to group the strings by their length, and return a map where the keys are the lengths and the values are lists of strings with that length.
-        *
-        * Given a list of strings, use the Stream API to group the strings by their first letter, and return a map where the keys are the first letters and the values are lists of strings that start with that letter.
+         * 3 Given a list of strings, use the Stream API to group the strings by their length, and return a map where the keys are the lengths and the values are lists of strings with that length.
+         *
+         * 4 Given a list of strings, use the Stream API to group the strings by their first letter, and return a map where the keys are the first letters and the values are lists of strings that start with that letter.
 
-        * Given a list of people, use the Stream API to group the people by their age, and return a map where the keys are the ages and the values are lists of people with that age.
+         * 5 Given a list of people, use the Stream API to group the people by their age, and return a map where the keys are the ages and the values are lists of people with that age.
 
-        * Given a list of strings, use the Stream API to filter out strings that have a length less than 4, reverse the remaining strings, and return a new list of the modified strings.
+         * 6 Given a list of strings, use the Stream API to filter out strings that have a length less than 4, reverse the remaining strings, and return a new list of the modified strings.
 
-        * Given a list of people, use the Stream API to filter out people who are older than 40, sort the remaining people by their age in descending order, and return a new list of the modified people.
-*/
+         * 7 Given a list of people, use the Stream API to filter out people who are older than 40, sort the remaining people by their age in descending order, and return a new list of the modified people.
+         */
+
+        List<Integer> intNumbers = List.of(1, 2, 5, 2, 6, 8, 3, 12, 34, 6, 7, 34, 56, 22, 4, 6, 7, 2, 12, 31, 23);
+        List<String> strings = List.of("jonathan", "daniel", "music", "drive", "keyboard", "mouse", "standing", "car", "bikers", "ball", "measurement", "one", "run");
+
+        People people1 = new People("Jonathan", 25, People.Gender.M);
+        People people2 = new People("Sandra", 30, People.Gender.F);
+        People people3 = new People("Antonio", 25, People.Gender.M);
+        People people4 = new People("Jorge", 17, People.Gender.M);
+        People people5 = new People("Mary", 20, People.Gender.F);
+        People people6 = new People("Paola", 20, People.Gender.F);
+        People people7 = new People("Camilo", 22, People.Gender.M);
+        People people8 = new People("Harry", 25, People.Gender.F);
+        People people9 = new People("Leo", 41, People.Gender.M);
+        People people10 = new People("Andres", 41, People.Gender.M);
+        People people11 = new People("Ban", 42, People.Gender.M);
+        People people12 = new People("Sarah", 45, People.Gender.F);
+        List<People> people = List.of(people1, people2, people3, people4, people5, people6, people7, people8, people9, people10, people11, people12);
+
         //1st
-        List<Integer> intNumbers = List.of(1,2,5,2,6,8,3,12,34,6,7,34,56,22,4,6,7,2,12,31,23);
-        //System.out.println("1ST TASK -- Sum of even filtered numbers squared: "+sumOfSquaredFilteredList(intNumbers));
+        System.out.println("1ST TASK -- Sum of even filtered numbers squared: "+sumOfSquaredFilteredList(intNumbers));
 
         //2nd
-        List<String> strings = List.of("jonathan", "daniel", "music", "drive", "keyboard", "mouse", "standing", "car", "bikers", "ball", "measurement");
-        //System.out.println("2ND TASK -- Filtered and capitalized strings: " + filterAndCapitalized(strings));
+        System.out.println("2ND TASK -- Filtered and capitalized strings: " + filterAndCapitalized(strings));
 
         //3rd
-        //System.out.println("3RD TASK -- Group by length: "+lengthMap(List.of("apple","melon","pineapple","mango","lemon","blueberry","banana","strawberry","kiwi","maracuya","dragon fruit")));
+        System.out.println("3RD TASK -- Group by length: "+lengthMap(List.of("apple","melon","pineapple","mango","lemon","blueberry","banana","strawberry","kiwi","maracuya","dragon fruit")));
 
         //4th
-        System.out.println("4TH TASK -- Map group by first letter"+ sameFirstLetter(strings));
+        System.out.println("4TH TASK -- Map group by first letter" + sameFirstLetter(strings));
+
+        //5th
+        System.out.println("5TH TASK -- Map group by Age: " + groupPeopleByAge(people));
+
+        //6th
+        System.out.println("6Th TASK -- List of reversed and filtered: " + filterAndReverse(strings));
+
+        //7th
+        System.out.println("7TH TASK -- List of sorted people with 40 years or more: " + filterAndSortPeople(people));
     }
 
     //method for the first task
-    public static Integer sumOfSquaredFilteredList(List<Integer> numbers){
+    public static Integer sumOfSquaredFilteredList(List<Integer> numbers) {
         return numbers.stream()
                 .filter(x -> x % 2 == 0)
-                .map(y->Math.pow(y,2))
+                .map(y -> Math.pow(y, 2))
                 .mapToInt(Double::intValue)
                 .sum();
     }
 
     //method for the second task
-    public static List<String> filterAndCapitalized(List<String> strings){
+    public static List<String> filterAndCapitalized(List<String> strings) {
         return strings.stream()
-                .filter((x) -> x.length() > 5)
+                .filter(x -> x.length() > 5)
                 .map(String::toUpperCase)
                 .collect(Collectors.toList());
     }
 
     //method for the third task
-    public static Map<Integer,List<String>> lengthMap(List<String> words){
+    public static Map<Integer, List<String>> lengthMap(List<String> words) {
         return words.stream()
                 .collect(Collectors.groupingBy(String::length));
     }
 
     //method for the fourth task
-    public static Map<String,List<String>> sameFirstLetter(List<String> words){
-        return words.stream().collect(Collectors.groupingBy( x -> String.valueOf(x.charAt(0))));
+    public static Map<String, List<String>> sameFirstLetter(List<String> words) {
+        return words.stream()
+                .collect(Collectors
+                        .groupingBy(x -> String.valueOf(x.charAt(0))));
+    }
+
+    //method for the fifth task
+    public static Map<Integer, List<People>> groupPeopleByAge(List<People> people) {
+        return people.stream().collect(Collectors.groupingBy(People::getAge));
+    }
+
+    //method for the sixth task
+    public static List<String> filterAndReverse(List<String> strings) {
+        return strings.stream()
+                .filter(x -> x.length() < 4)
+                .map(x -> new StringBuilder(x).reverse().toString())
+                .collect(Collectors.toList());
+    }
+
+    //method for the seventh task
+    public static List<People> filterAndSortPeople(List<People> people) {
+        return people.stream()
+                .filter(x -> x.getAge() > 40)
+                .sorted(Comparator.comparing(People::getAge))
+                .collect(Collectors.toList());
     }
 }
