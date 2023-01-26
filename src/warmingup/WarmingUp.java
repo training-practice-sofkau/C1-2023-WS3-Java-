b767e432fde27fc4d7bf689110b684932acbd713
BrianSammit <briansammit.cruz@gmail.com>
2023-01-26 13:16:56 -0500

    Add function to group words by first letter of the word

--- src/warmingup/WarmingUp.java
+++ src/warmingup/WarmingUp.java
@@ -12,9 +12,12 @@ public class WarmingUp {
         System.out.println(upCaseLength(List.of("apple","melon","pineapple","mango","lemon","blueberry","banana","strawberry","kiwi","maracuya","pitahaya")));
 
 //      Given a list of strings, use the Stream API to group the strings by their length, and return a map where the keys are the lengths and the values are lists of strings with that length.
+        System.out.println(firstLeterMap(List.of("apple","melon","pineapple","mango","lemon","blueberry","banana","strawberry","kiwi","maracuya","pitahaya")));
+
+//      Given a list of strings, use the Stream API to group the strings by their first letter, and return a map where the keys are the first letters and the values are lists of strings that start with that letter.
         System.out.println(lengthMap(List.of("apple","melon","pineapple","mango","lemon","blueberry","banana","strawberry","kiwi","maracuya","pitahaya")));
+
         /* TO DO:
-        * Given a list of strings, use the Stream API to group the strings by their first letter, and return a map where the keys are the first letters and the values are lists of strings that start with that letter.
 
         * Given a list of people, use the Stream API to group the people by their age, and return a map where the keys are the ages and the values are lists of people with that age.
 
@@ -33,6 +36,11 @@ public class WarmingUp {
     public static List<String> upCaseLength(List<String> words){
         //First I need to know the length of the words
         return words.stream().filter(w -> w.length() > 5).map(String::toUpperCase).collect(Collectors.toList());
+
+    }
+    public static Map<Character,List<String>> firstLeterMap(List<String> words){
+        //First I need to know the length of the words
+        return words.stream().collect(Collectors.groupingBy(s -> s.charAt(0)));
 
     }
     public static Map<Integer,List<String>> lengthMap(List<String> words){
