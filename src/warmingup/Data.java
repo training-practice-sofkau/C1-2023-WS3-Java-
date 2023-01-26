231dff5863df1ad00272169dc60f544b01fee786
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-01-26 13:52:01 -0500

    Finish Warming Up 2

--- src/warmingup/Data.java
+++ src/warmingup/Data.java
@@ -6,4 +6,6 @@ public class Data {
 
     public static List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
 
+    public static List<String> words = List.of("Wish", "Seventeen", "Rust", "Down", "Oven", "Nine", "Hearth", "Homecoming", "One", "Wagon");
+
 }
--- src/warmingup/WarmingUp.java
+++ src/warmingup/WarmingUp.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 
 public class WarmingUp {
     public static void main(String[] args) {
-        System.out.println(lengthMap(List.of("apple","melon","pinneapple","mango","lemon","blueberry","banana","strawberry","kiwi","maracuya","pitahaya")));
+
         /* TO DO:
         * Given a list of integers, use the Stream API to filter out even numbers, square the remaining numbers, and return the sum of the squared numbers.
         */
@@ -16,9 +16,17 @@ public class WarmingUp {
 
         /*
         * Given a list of strings, use the Stream API to filter out strings that have a length greater than 5, convert the remaining strings to uppercase, and return a new list of the modified strings.
+        */
 
+        //System.out.println("\nOriginal list: " + Data.words + "\nFiltered list: " +longWordsFilter(Data.words) + "\n");
+
+        /*
         * Given a list of strings, use the Stream API to group the strings by their length, and return a map where the keys are the lengths and the values are lists of strings with that length.
+        */
 
+        lengthMap(Data.words).forEach((key, values) -> System.out.println("" + key + values));
+
+        /*
         * Given a list of strings, use the Stream API to group the strings by their first letter, and return a map where the keys are the first letters and the values are lists of strings that start with that letter.
 
         * Given a list of people, use the Stream API to group the people by their age, and return a map where the keys are the ages and the values are lists of people with that age.
@@ -27,6 +35,20 @@ public class WarmingUp {
 
         * Given a list of people, use the Stream API to filter out people who are older than 40, sort the remaining people by their age in descending order, and return a new list of the modified people.
 */
+    }
+
+    //public static Map<Integer,List<String>> lengthMap(List<String> words){
+    //    //First I need to know the length of the words
+    //    return words.stream()
+    //            .collect(Collectors.groupingBy(String::length));
+    //}
+
+    public static List<String> longWordsFilter(List<String> words){
+
+        return words.stream()
+                .filter(s -> (s.length() <= 5))
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
     }
 
     public static int sumSquares(List<Integer> numbers) {
@@ -44,9 +66,4 @@ public class WarmingUp {
         return result;
     }
 
-    public static Map<Integer,List<String>> lengthMap(List<String> words){
-        //First I need to know the length of the words
-        return words.stream().collect(Collectors.groupingBy(String::length));
-
-    }
 }
