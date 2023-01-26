0aacd5df4616a67abe4a3b2e24a351cb78fcdaee
Julian Rojas <julianrojasgeo@gmail.com>
2023-01-26 13:43:44 -0500

    Exercise 1

--- src/warmingup/WarmingUp.java
+++ src/warmingup/WarmingUp.java
@@ -8,27 +8,40 @@ import java.util.stream.Collectors;
 
 public class WarmingUp {
     public static void main(String[] args) {
-        System.out.println(lengthMap(List.of("apple","melon","pinneapple","mango","lemon","blueberry","banana","strawberry","kiwi","maracuya","pitahaya")));
+
         /* TO DO:
-        * Given a list of integers, use the Stream API to filter out even numbers, square the remaining numbers, and return the sum of the squared numbers.
+        * 1. Given a list of integers, use the Stream API to filter out even numbers, square the remaining numbers, and return the sum of the squared numbers.
 
-        * Given a list of strings, use the Stream API to filter out strings that have a length greater than 5, convert the remaining strings to uppercase, and return a new list of the modified strings.
+        * 2. Given a list of strings, use the Stream API to filter out strings that have a length greater than 5, convert the remaining strings to uppercase, and return a new list of the modified strings.
 
-        * Given a list of strings, use the Stream API to group the strings by their length, and return a map where the keys are the lengths and the values are lists of strings with that length.
+        * 3. Given a list of strings, use the Stream API to group the strings by their length, and return a map where the keys are the lengths and the values are lists of strings with that length.
 
-        * Given a list of strings, use the Stream API to group the strings by their first letter, and return a map where the keys are the first letters and the values are lists of strings that start with that letter.
+        * 4. Given a list of strings, use the Stream API to group the strings by their first letter, and return a map where the keys are the first letters and the values are lists of strings that start with that letter.
 
-        * Given a list of people, use the Stream API to group the people by their age, and return a map where the keys are the ages and the values are lists of people with that age.
+        * 5. Given a list of people, use the Stream API to group the people by their age, and return a map where the keys are the ages and the values are lists of people with that age.
 
-        * Given a list of strings, use the Stream API to filter out strings that have a length less than 4, reverse the remaining strings, and return a new list of the modified strings.
+        * 6. Given a list of strings, use the Stream API to filter out strings that have a length less than 4, reverse the remaining strings, and return a new list of the modified strings.
 
-        * Given a list of people, use the Stream API to filter out people who are older than 40, sort the remaining people by their age in descending order, and return a new list of the modified people.
+        * 7. Given a list of people, use the Stream API to filter out people who are older than 40, sort the remaining people by their age in descending order, and return a new list of the modified people.
 */
+        //Ex1
+        System.out.println("Exercise 1.");
+        System.out.println(ex1EvenNumbers(List.of(1,2,1,2,1,2,1,2,1,10)));
+
+        //Ex3
+        System.out.println("Exercise 3:");
+        System.out.println(ex3LengthMap(List.of("apple","melon","pinneapple","mango","lemon","blueberry","banana","strawberry","kiwi","maracuya","pitahaya")));
     }
 
-    public static Map<Integer,List<String>> lengthMap(List<String> words){
+    public static Integer ex1EvenNumbers(List<Integer> numbers ){
+
+        return numbers.stream().filter(x -> x%2==0).map(x -> x*x).mapToInt(Integer::valueOf).sum();
+
+
+    }
+
+    public static Map<Integer,List<String>> ex3LengthMap(List<String> words){
         //First I need to know the length of the words
         return words.stream().collect(Collectors.groupingBy(String::length));
-
     }
 }
