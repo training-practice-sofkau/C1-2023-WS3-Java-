8f01894633cebc08344d419681e6e7ccf28857bc
isan-san <isan.9.9f@gmail.com>
2023-01-26 16:46:07 -0500

    Checked People class

--- src/warmingup/People.java
+++ src/warmingup/People.java
@@ -1,7 +1,28 @@
 package warmingup;
 
-public class People {
-    private String name;
-    private int age;
-    private String gender; //M, F
+public final class People {
+    private final String name;
+    private final int age;
+    private final String gender; //M, F
+
+    public People(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+
+
+    public String getGender() {
+        return gender;
+    }
+
 }
