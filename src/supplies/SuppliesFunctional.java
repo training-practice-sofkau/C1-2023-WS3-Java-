90f596db0b62b1519321a87f3e45d7cac7681305
Difsan <difsanchezru@gmail.com>
2023-01-26 17:22:41 -0500

    getNYSales and lessSatisfaction were done

--- src/supplies/SuppliesFunctional.java
+++ src/supplies/SuppliesFunctional.java
@@ -1,6 +1,7 @@
 package supplies;
 
 
+import supplies.sales.Customer;
 import supplies.sales.Database;
 import supplies.sales.Product;
 import supplies.sales.Sale;
@@ -12,8 +13,8 @@ import java.util.stream.Collectors;
 public class SuppliesFunctional {
     static ArrayList<Sale> sales = Database.loadDatabase();
     public static void main(String[] args) {
-        //loadMenu();
-        sales.forEach(System.out::println);
+        loadMenu();
+        //sales.forEach(System.out::println);
 
     }
 
@@ -35,13 +36,13 @@ public class SuppliesFunctional {
         String op=sc.nextLine();
         switch(op){
             case "1":
-
+                System.out.println(getNYSales());
                 break;
             case "2":
-
+                System.out.println(lessSatisfaction());
                 break;
             case "3":
-
+                System.out.println(test());
                 break;
             case "4":
 
@@ -53,6 +54,38 @@ public class SuppliesFunctional {
                 System.out.println("Invalid input. Try again.");
         }
 
+    }
+
+    // 1st excercise
+    public static Map<String, List<Sale>> getNYSales (){
+
+        Map<String, List<Sale>> map1 = new HashMap<>();
+        map1.put("Used", null);
+        map1.put("No Used", null);
+        map1.replace("Used", sales.stream().
+                filter(i -> i.getLocation().equals("New York")).
+                collect(Collectors.toList()).
+                stream().filter(i->i.getCouponUsed()).collect(Collectors.toList()));
+        map1.replace("No Used", sales.stream().
+                filter(i -> i.getLocation().equals("New York")).
+                collect(Collectors.toList()).
+                stream().filter(i->!i.getCouponUsed()).collect(Collectors.toList()));
+        return map1;
+    }
+
+    // 1nd excercise
+    public static List<Customer> lessSatisfaction (){
+        List <Customer> list = new ArrayList<>();
+        sales.stream().forEach(i->list.add(i.getCustomer()));
+
+        return list.stream().
+                filter(i->i.getSatisfaction()==list.get(0).getSatisfaction()).
+                collect(Collectors.toList()).stream().
+                sorted(Comparator.comparingInt(Customer::getAge).reversed()).collect(Collectors.toList());
+    }
+
+    public static List<Customer> test(){
+        return null;
     }
 
 
